78bab5393057e1777eaeb4c4cba1a4c04542bd98
Bianca Rodrigues <binaksrc@gmail.com>
2018-10-07 12:25:01 -0300

    complete KenoBet.h

--- include/KenoBet.h
+++ include/KenoBet.h
@@ -15,6 +15,31 @@ class KenoBet {
             @return T if number chosen is successfully inserted; F otherwise. */
         bool add_number( number_type spot_ );
 
+        /*! Sets the amount of money the player is betting
+            @param wage_ The wage.
+            @return True if we have a wage above zero; false otherwise. */
+        bool set_wage( cash_type wage_);
+
+        //! Resets a bet to an empty state.
+        void reset( void );
+
+        /*! Retrieves the player's wage on this bet.
+            @return The wage value. */
+        cash_type get_wage( void ) const;
+
+        /*! Returns the current number of spots in the player's bet.
+            @return Number of spots present in the bet. */
+        size_t size( void ) const;
+
+        /*! Determine how many spots match the hits passed as argument.
+            @param hits_ List of hits randomly chosen by the computer.
+            @return A vector with the list of hits. */
+        set_of_numbers_type get_hits( const set_of_numbers_type & hits_ ) const;
+
+        /*! Return a vector< spot_type > with the spots the player has picked so far.
+            @return The vector< spot_type > with the player's spots picked so far. */
+        set_of_numbers_type get_spots( void ) const;
+
     private:
         set_of_numbers_type m_spots; // <! The player's bet
         cash_type m_wage;            // <! The player's wage
